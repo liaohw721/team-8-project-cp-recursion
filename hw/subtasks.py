47652da5b4fab4c86324b07a030690846748189c
r14945024 <r14945024bebi@gmail.com>
2025-10-23 21:51:32 +0800

    change the score in hw

--- hw/subtasks.py
+++ hw/subtasks.py
@@ -4,9 +4,9 @@
     [5, ('testcase/03.in', 'answer/03.out', 1.0, 128, 0)],
     [5, ('testcase/04.in', 'answer/04.out', 1.0, 128, 0)],
     [5, ('testcase/05.in', 'answer/05.out', 1.0, 128, 0)],
-    [5, ('testcase/06.in', 'answer/06.out', 1.0, 128, 0)],
-    [5, ('testcase/07.in', 'answer/07.out', 1.0, 128, 0)],
-    [5, ('testcase/08.in', 'answer/08.out', 1.0, 128, 0)],
-    [5, ('testcase/09.in', 'answer/09.out', 1.0, 128, 0)],
-    [5, ('testcase/10.in', 'answer/10.out', 1.0, 128, 0)]
+    [10, ('testcase/06.in', 'answer/06.out', 1.0, 128, 0)],
+    [15, ('testcase/07.in', 'answer/07.out', 1.0, 128, 0)],
+    [15, ('testcase/08.in', 'answer/08.out', 1.0, 128, 0)],
+    [15, ('testcase/09.in', 'answer/09.out', 1.0, 128, 0)],
+    [20, ('testcase/10.in', 'answer/10.out', 1.0, 128, 0)]
 ]
